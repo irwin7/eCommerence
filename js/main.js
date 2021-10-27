8209ba18266677d135d4d1074984d98663a02101
irwin7 <ravshankhan777@icloud.com>
2021-10-27 22:59:26 +0500

    pushed

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
 let elBurgerBtn = document.querySelector(".burger")
 let elMenuBtn = document.querySelector(".burger__menu")
-let elDownSect = document.querySelector(".down-sect")
+let elGridBtn = document.querySelector(".grid")
+let elListBtn = document.querySelector(".list")
+let elOpenFilter = document.querySelector(".filter__open-btn")
+let elCloseFilter = document.querySelector(".filters__close")
+let elFilter = document.querySelector(".filters")
+let elProductBox = document.querySelector(".products__list")
 
+elCloseFilter.addEventListener("click", ()=>{
+  elFilter.classList.remove("simsim");
+})
+elOpenFilter.addEventListener("click", ()=>{
+  elFilter.classList.add("simsim");
+})
 elBurgerBtn.addEventListener("click", ()=>{
   elMenuBtn.classList.toggle("open");
   elDownSect.classList.toggle("open");
+})
+elGridBtn.addEventListener("click", ()=>{
+  elProductBox.classList.add("open");
+  elListBtn.classList.remove("active");
+  elGridBtn.classList.add("active");
+})
+elListBtn.addEventListener("click", ()=>{
+  elProductBox.classList.remove("open");
+  elGridBtn.classList.remove("active");
+  elListBtn.classList.add("active");
 })
